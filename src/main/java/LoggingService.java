b50240d989ee0468516fb6fafd5b445a3cbcc7c1
tavinogradova92 <tavinogradova92@gmail.com>
2020-10-10 14:27:51 +0200

    Logging Service Formatter added

--- src/main/java/LoggingService.java
+++ src/main/java/LoggingService.java
@@ -3,18 +3,25 @@ import java.util.logging.Logger;
 import java.util.logging.FileHandler;
 import java.lang.SecurityException;
 import java.io.IOException;
+import java.util.logging.SimpleFormatter;
 
 public abstract class LoggingService {
     
     public static void executeLogging() {
-         
+        
+        Logger logger = Logger.getLogger("MyLog");  
+        // This block configure the logger with handler and formatter  
+        boolean append = true;
+        FileHandler fh;
     
         try {  
-            Logger logger = Logger.getLogger("MyLog");  
-            // This block configure the logger with handler and formatter  
-            boolean append = true;
-            FileHandler fh = new FileHandler("./src/test/MyLogFile.log", append);  
+            
+            fh = new FileHandler("./src/test/MyLogFile.log", append);  
             logger.addHandler(fh);  
+
+            // Print a brief summary of the LogRecord in a human readable format.
+			SimpleFormatter formatter = new SimpleFormatter();	
+			fh.setFormatter(formatter);
     
             // the following statement is used to log any messages  
             logger.info("My first log");  
